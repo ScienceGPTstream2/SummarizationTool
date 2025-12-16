bb35faf7fb0b12b36e3ffdb68fafc99a2d0e1825
Ubuntu <azureuser@summarize-t4gpu.w0onxwzgxsnuvl2kzxz53cwqng.ux.internal.cloudapp.net>
2025-12-16 21:39:00 +0000

    Updated templates

--- components/TemplateLoader.tsx
+++ components/TemplateLoader.tsx
@@ -4,8 +4,14 @@ import { metaAnalysisTemplate } from "../templates/meta-analysis";
 import { caseStudyTemplate } from "../templates/case-study";
 import { reviewTemplate } from "../templates/review";
 import { level1InVivoTemplate } from "../templates/level-1-in-vivo";
+import { level1InVivoNoShotTemplate } from "../templates/level-1-in-vivo-noshot";
 import { level2InVivoTemplate } from "../templates/level-2-in-vivo";
+import { level2InVivoNoShotTemplate } from "../templates/level-2-in-vivo-noshot";
 import { level1EpidemiologyTemplate } from "../templates/level-1-epidemiology";
+import { generalLevel1EpidemiologyAdamV1Template } from "../templates/general-level-1-epidemiology-adam-v1";
+import { generalLevel1EpidemiologyAdamV1NoShotTemplate } from "../templates/general-level-1-epidemiology-adam-v1-noshot";
+import { level0EpidemiologyMetaDataTemplate } from "../templates/level-0-epidemiology-metadata";
+import { level0EpidemiologyMetaDataNoShotTemplate } from "../templates/level-0-epidemiology-metadata-noshot";
 
 export interface EntityTemplate {
   name: string;
@@ -26,8 +32,16 @@ const templates: Record<string, StudyTypeTemplate> = {
   "case-study": caseStudyTemplate,
   review: reviewTemplate,
   "level-1-in-vivo": level1InVivoTemplate,
+  "level-1-in-vivo-noshot": level1InVivoNoShotTemplate,
   "level-2-in-vivo": level2InVivoTemplate,
+  "level-2-in-vivo-noshot": level2InVivoNoShotTemplate,
   "level-1-epidemiology": level1EpidemiologyTemplate,
+  "General-level-1-epidemiology-AdamV1": generalLevel1EpidemiologyAdamV1Template,
+  "General-level-1-epidemiology-AdamV1-NoShot":
+    generalLevel1EpidemiologyAdamV1NoShotTemplate,
+  "Level-0-epidemiology-MetaData": level0EpidemiologyMetaDataTemplate,
+  "Level-0-epidemiology-MetaData-NoShot":
+    level0EpidemiologyMetaDataNoShotTemplate,
 };
 
 export const loadStudyTypeTemplate = (
