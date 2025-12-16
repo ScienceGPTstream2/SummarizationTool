511ed32e2126d2d3b70d465e83bd16b433bcd1d5
Ubuntu <azureuser@summarize-t4gpu.w0onxwzgxsnuvl2kzxz53cwqng.ux.internal.cloudapp.net>
2025-12-16 21:44:53 +0000

    formatted with prettier

--- templates/level-0-epidemiology-metadata-noshot.ts
+++ templates/level-0-epidemiology-metadata-noshot.ts
@@ -4,23 +4,28 @@ export const level0EpidemiologyMetaDataNoShotTemplate = {
   entities: [
     {
       name: "Study Author(s)",
-      prompt: "Extract the study author(s). If not reported, output \"Not Reported.\""
+      prompt:
+        'Extract the study author(s). If not reported, output "Not Reported."',
     },
     {
       name: "Author Affiliations",
-      prompt: "Extract the affiliations of the authors. If not reported, output \"Not Reported.\""
+      prompt:
+        'Extract the affiliations of the authors. If not reported, output "Not Reported."',
     },
     {
       name: "Study Title",
-      prompt: "Extract the title of the study. If not reported, output \"Not Reported.\""
+      prompt:
+        'Extract the title of the study. If not reported, output "Not Reported."',
     },
     {
       name: "Publication Date",
-      prompt: "Extract the publication date of the study. If not reported, output \"Not Reported.\""
+      prompt:
+        'Extract the publication date of the study. If not reported, output "Not Reported."',
     },
     {
       name: "Journal",
-      prompt: "Extract the name of the journal where the study was published. If not reported, output \"Not Reported.\""
-    }
-  ]
+      prompt:
+        'Extract the name of the journal where the study was published. If not reported, output "Not Reported."',
+    },
+  ],
 };
