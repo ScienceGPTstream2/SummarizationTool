6d621e93e07f0050c34b739fd59ef056b92b4e33
Ubuntu <azureuser@Deploy-A100.4hnupjsvrlre1k2rq10gsqgpnc.cx.internal.cloudapp.net>
2026-03-27 21:33:46 +0000

    Add user-created prompt templates to static templates

---
+++ templates/tester.ts
@@ -0,0 +1,11 @@
+export const testerTemplate = {
+  "studyType": "tester",
+  "displayName": "tester",
+  "entities": [
+    {
+      "name": "what model are u",
+      "prompt": "what model are you"
+    }
+  ],
+  "summaryPrompt": "Take the extracted entities and put them into a single cohesive paragraph. Do not change any of the extracted entities."
+};
